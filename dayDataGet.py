89bb76a6b73f9f0904a6d24147a85e002ad1a989
badroot <teflox@gmail.com>
2017-11-12 04:40:19 -0500

    fix dayDataGet

--- dayDataGet.py
+++ dayDataGet.py
@@ -23,6 +23,13 @@ if os.path.exists('dayData.txt'):
 
     if( 0 == absTime-data['latest'] ):
         time.sleep(60)
+else:
+    req = requests.get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+    dataNew = req.json()
+    timeT = (dataNew['time']['updated'].split())[3].split(':')
+    absTime = (int(timeT[0]) * 60) + int(timeT[1])
+    data = {'start': absTime, 'latest': absTime, 'price': []}
+    
 while True:
     req = requests.get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
     dataNew = req.json()

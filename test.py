e14f03a3a85a5428bf3378d40bb162e7f7637221
Eric Meltzer <meltzer.eric@live.com>
2017-11-12 04:08:33 -0500

    add multiprocessing

--- test.py
+++ test.py
@@ -4,28 +4,50 @@ from matplotlib.figure import Figure
 import matplotlib as plt
 import numpy as np
 import requests
-
+from datetime import *
+import time
+from multiprocessing import Process, Pipe
 app = QApplication([])
 w = QWidget()
 layout = QGridLayout(w)
 
+def f():
+    while x:
+        print(x)
+
+p = Process(target=f)
+
 def rangeRequest(startStr, endStr):
     resp = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start='+startStr+'&end='+endStr)
-    #print(resp.)
     dataSet = resp.json()
     return dataSet
 
 def arrToGraph(datDict):
     x = np.linspace(0, len(datDict['bpi']), len(datDict['bpi']))
-    y = datDict['bpi'].values()
-    plt.plot(x,y)
+    y = sorted(datDict['bpi'])
+    y = [datDict['bpi'][k] for k in y]
+    ax.clear()
+    ax.plot(x,y)
 
 def selectionchanged():
-    print(cb.currentText())
-    lst.append(lst[-1]+1)
-    print(lst)
-    ax.clear()
-    ax.plot(lst, '+-')
+    global x
+    x = False
+    parent_conn.send(x)
+    end = datetime.today()-timedelta(days=1)
+    s = str(cb.currentText())
+    options = {'7D':timedelta(weeks=1), '1M':timedelta(weeks=4), '3M':timedelta(weeks=12), '1Y':timedelta(weeks=52)}
+    endStr = datetime.strftime(end, '%Y-%m-%d')
+    if s in options:
+        delta = options[s]
+        startStr = datetime.strftime((end-delta), '%Y-%m-%d')
+        print(startStr,' ', endStr)
+        arrToGraph(rangeRequest(startStr, endStr))
+    elif s == 'ALL':
+        arrToGraph(rangeRequest('2010-07-19', endStr))
+    else:
+        x = True
+        if not p.is_alive():
+            p.start()
     canvas = FigureCanvas(figure)
     layout.addWidget(canvas, 0, 0)
     w.update()
@@ -41,8 +63,6 @@ figure = Figure()
 canvas = FigureCanvas(figure)
 ax = figure.gca()
 ax.clear()
-lst=[1,2,3]
-ax.plot(lst, '+-')
 layout.addWidget(canvas, 0, 0)
 layout.addWidget(cb, 1, 0)
 
